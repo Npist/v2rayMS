f67646b53c9b5ed369c25fd2443b8f380872bfc7
Npist <npist35@gmail.com>
2018-08-03 23:03:32 +0800

    添加SQL文件

--- README.md
+++ README.md
@@ -1,47 +1,26 @@
-仅供测试使用
-
-有问题请联系npist35@gmail.com
-
-
-更新日志
-
-2018.8.1 
-
-初始版本
-
-
-v2ray多用户后端
-
-建议使用Python3.6运行
-
-节点服务器运行前请参考官方文档安装v2ray
-
-安装命令行如下
-
-bash <(curl -L -s https://install.direct/go.sh)
-
-
-数据库服务器存放以下文件
-
-v2rayMS_Server.py
-
-v2server.py
-
-sqlconn.json
-
-sqlconn.json为数据库连接文件  根据自己的mysql数据库配置
-
-执行以下命令启动
-
-nohup python3 v2rayMS_Server.py>> server.log 2>&1 &
-
-
-节点服务器存放以下文件
-
-v2rayMS_Client.py
-
-打开文件  修改服务端IP
-
-执行以下命令启动
-
-nohup python3 v2rayMS_Client.py>> server.log 2>&1 &
+仅供测试使用<br />
+有问题请联系npist35@gmail.com<br />
+<br />
+更新日志<br />
+2018.8.1<br />
+初始版本<br />
+<br />
+v2ray多用户后端<br />
+建议使用Python3.6运行<br />
+节点服务器运行前请参考官方文档安装v2ray<br />
+安装命令行如下<br />
+bash <(curl -L -s https://install.direct/go.sh)<br />
+<br />
+数据库服务器存放以下文件<br />
+v2rayMS_Server.py<br />
+v2server.py<br />
+sqlconn.json<br />
+sqlconn.json为数据库连接文件  根据自己的mysql数据库配置<br />
+执行以下命令启动<br />
+nohup python3 v2rayMS_Server.py>> server.log 2>&1 &<br />
+<br />
+节点服务器存放以下文件<br />
+v2rayMS_Client.py<br />
+打开文件  修改服务端IP<br />
+执行以下命令启动<br />
+nohup python3 v2rayMS_Client.py>> server.log 2>&1 &<br />
---
+++ v2ray.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 2018-08-03 14:59:53
+-- 服务器版本： 10.3.8-MariaDB-log
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `v2ray`
+--
+CREATE DATABASE IF NOT EXISTS `v2ray` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `v2ray`;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `uuid` varchar(36) NOT NULL,
+  `t` int(11) NOT NULL DEFAULT 0,
+  `u` bigint(20) NOT NULL,
+  `d` bigint(20) NOT NULL,
+  `transfer_enable` bigint(20) NOT NULL,
+  `enable` tinyint(4) NOT NULL DEFAULT 1,
+  `created_at` int(10) NOT NULL,
+  `updated_at` int(10) NOT NULL,
+  `need_reset` tinyint(1) NOT NULL DEFAULT 1,
+  `sid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`,`email`,`uuid`,`sid`) USING BTREE;
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
