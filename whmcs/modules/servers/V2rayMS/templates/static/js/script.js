d9ecda00767dfd002918eac3e5dd45e460325d75
mlge <admin@mlge.xyz>
2018-10-02 13:20:59 +0800

    添加base64编码的中文支持
    添加base64编码的中文支持

--- whmcs/modules/servers/V2rayMS/templates/static/js/script.js
+++ whmcs/modules/servers/V2rayMS/templates/static/js/script.js
@@ -1,34 +1,34 @@
 var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 function base64encode(str) {
-	var out, i, len;
-	var c1, c2, c3;
-	str = utf8_encode(str);
-	len = str.length;
-	i = 0;
-	out = "";
-	while (i < len) {
-		c1 = str.charCodeAt(i++) & 0xff;
-		if (i == len) {
-			out += base64EncodeChars.charAt(c1 >> 2);
-			out += base64EncodeChars.charAt((c1 & 0x3) << 4);
-			out += "==";
-			break;
-		}
-		c2 = str.charCodeAt(i++);
-		if (i == len) {
-			out += base64EncodeChars.charAt(c1 >> 2);
-			out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
-			out += base64EncodeChars.charAt((c2 & 0xF) << 2);
-			out += "=";
-			break;
-		}
-		c3 = str.charCodeAt(i++);
-		out += base64EncodeChars.charAt(c1 >> 2);
-		out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
-		out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
-		out += base64EncodeChars.charAt(c3 & 0x3F);
-	}
-	return out;
+    var out, i, len;
+    var c1, c2, c3;
+    str = utf8_encode(str);
+    len = str.length;
+    i = 0;
+    out = "";
+    while (i < len) {
+        c1 = str.charCodeAt(i++) & 0xff;
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+            out += "==";
+            break;
+        }
+        c2 = str.charCodeAt(i++);
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+            out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+            out += "=";
+            break;
+        }
+        c3 = str.charCodeAt(i++);
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+        out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+        out += base64EncodeChars.charAt(c3 & 0x3F);
+    }
+    return out;
 }
 function utf8_encode(str) {
 	str = str.replace(/\r\n/g, "\n");
